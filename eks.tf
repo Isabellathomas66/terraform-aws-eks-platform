ca5989a47f561b00e7c4fd49350e744b2b1212d5
Isabellathomas66 <thomas.isabella66@gmail.com>
2026-05-13 12:45:19 -0500

    Create eks.tf

---
+++ eks.tf
@@ -0,0 +1,15 @@
+resource "aws_eks_cluster" "cluster" {
+  name     = "devops-eks-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.public.id,
+      aws_subnet.private.id
+    ]
+  }
+
+  depends_on = [
+    aws_internet_gateway.igw
+  ]
+}
